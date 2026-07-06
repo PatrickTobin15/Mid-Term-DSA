b07839ea3ee771076e08b166d28696791ede7c18
PatrickTobin15 <patrick.tobin@keyin.com>
2026-07-06 16:19:38 -0230

    Update Patient.java

--- src/main/java/com/patientms/Patient.java
+++ src/main/java/com/patientms/Patient.java
@@ -1,1 +1,32 @@
+package com.patientms;
 
+// Represents one patient in the waiting queue
+public class Patient {
+
+    private String patientId;
+    private String name;
+    private String reasonForVisit;
+
+    public Patient(String patientId, String name, String reasonForVisit) {
+        this.patientId = patientId;
+        this.name = name;
+        this.reasonForVisit = reasonForVisit;
+    }
+
+    public String getPatientId() {
+        return patientId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getReasonForVisit() {
+        return reasonForVisit;
+    }
+
+    @Override
+    public String toString() {
+        return "Patient ID: " + patientId + " | Name: " + name + " | Reason: " + reasonForVisit;
+    }
+}
